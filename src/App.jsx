9df9aac7ad6d393d5f93be17cf5bd9e97bdc8ed5
HassaaNaeem <hassaan282006@gmail.com>
2026-03-16 20:21:29 +0500

    Fetched cities data from API

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,30 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const BASE_URL = "http://localhost:8000";
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("Error loading cities data");
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
   return (
     <>
       <BrowserRouter>
@@ -16,8 +38,8 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>List of cities</p>}></Route>
-            <Route path="cities" element={<p>List of cities</p>}></Route>
+            <Route index element={<CityList />}></Route>
+            <Route path="cities" element={<CityList />}></Route>
             <Route path="countries" element={<p>List of countries</p>}></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return <ul className={styles.CityList}>Something</ul>;
+}
+
+export default CityList;
