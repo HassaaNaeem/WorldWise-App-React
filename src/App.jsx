111916cad2b556d3274b3bdc0663668970e938ef
HassaaNaeem <hassaan282006@gmail.com>
2026-03-17 00:00:52 +0500

    Implemented Country List

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Homepage from "./pages/Homepage";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
   const BASE_URL = "http://localhost:8000";
   useEffect(() => {
     async function fetchCities() {
@@ -46,7 +48,10 @@ function App() {
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             ></Route>
-            <Route path="countries" element={<p>List of countries</p>}></Route>
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            ></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
           <Route path="*" element={<PageNotFound />} />
