25e3110542df9e333011ed6d92f23bddccd0d712
HassaaNaeem <hassaan282006@gmail.com>
2026-03-17 20:54:36 +0500

    Added naviation using useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -54,7 +55,7 @@ function App() {
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             ></Route>
-            <Route path="form" element={<p>Form</p>}></Route>
+            <Route path="form" element={<Form />}></Route>
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
